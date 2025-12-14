1e545f86113c9f2cc2928c9cc5020dde4ee77fcc
Shahadat SDT <developer.shahadat@gmail.com>
2025-12-15 02:50:01 +0600

    color mode change

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import './App.css'
 import {Grid, GridItem} from "@chakra-ui/react";
 import Navbar from "@/components/NavBar.tsx";
+import GameGrid from "@/components/GameGrid.tsx";
 
 function App() {
 
     return (
-        <Grid templateAreas={{
-            base: '"nav" "main"',
-            lg: '"nav nav" "aside main"'
-        }}>
-            <GridItem area={"nav"} >
+        <Grid templateAreas={{base: '"nav" "main"', lg: '"nav nav" "aside main"'}}>
+            <GridItem area={"nav"}>
                 <Navbar/>
             </GridItem>
-            <GridItem area={"aside"} bg={"gold"} display={{base: "none", lg: "Block"}} >
+            <GridItem area={"aside"} display={{base: "none", lg: "Block"}}>
                 aside
             </GridItem>
-            <GridItem area={"main"} bg={"blue.50"}>
-                main
+            <GridItem area={"main"}>
+                <GameGrid/>
             </GridItem>
 
 
---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,9 @@
+import {ColorModeButton} from "@/components/ui/color-mode.tsx";
+
+const ColorModeSwitch = () => {
+ return (
+    <ColorModeButton/>
+  );
+};
+
+export default ColorModeSwitch;
